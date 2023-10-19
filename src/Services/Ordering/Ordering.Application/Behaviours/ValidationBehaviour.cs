0259f75f83c1024f371291f9dc2b6ecf9283dc60
Dang Giang <dangvngiang@gmail.com>
2023-10-19 23:29:11 +0700

    19-10-2023

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = Ordering.Application.Exceptions.ValidationException;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _valiators;
+
+        public ValidationBehaviour(IEnumerable<IValidator<TRequest>> valiators)
+        {
+            _valiators = valiators ?? throw new ArgumentNullException(nameof(valiators));
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (_valiators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+
+                var validationResults = await Task.WhenAll(_valiators.Select(v => v.ValidateAsync(context, cancellationToken)));
+                var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+
+                if (failures.Any())
+                {
+                    throw new ValidationException(failures);
+                }
+
+            }
+
+            return await next();
+        }
+    }
+}
